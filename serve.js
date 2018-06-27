9cb833e926e195b1c18046b4eb866a60abdda974
SherryH <enjoy.wind7rain@gmail.com>
2018-06-27 20:20:54 +1200

    Included schema

--- serve.js
+++ serve.js
@@ -1,6 +1,28 @@
 const express = require("express");
-
+const graphqlHTTP = require("express-graphql"); //GraphQL HTTP Server Middleware
+const { buildSchema } = require("graphql");
 const app = express();
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+    pretty: true,
+    formatError: error => ({
+      message: error.message,
+      locations: error.locations,
+      stack: error.stack ? error.stack.split("\n") : [],
+      path: error.path
+    })
+  })
+);
 
 app.listen("4000");
 console.log("listening...");
