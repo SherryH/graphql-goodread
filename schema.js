d705357817fb572ac7975ee968e1958ac8f0a94e
SherryH <enjoy.wind7rain@gmail.com>
2018-06-29 23:57:57 +1200

    Checked api key

---
+++ schema.js
@@ -0,0 +1,12 @@
+// schema: define the shape of the data returned by the query
+
+// not sure why we are fetching the goodreads info into schema, just follow video first
+const fetch = require("node-fetch");
+const GOODREADS_API_KEY = require("./config");
+console.log("api key", GOODREADS_API_KEY);
+// https://www.goodreads.com/api/index#author.show
+// get info about an author by id
+fetch(
+  `https://www.goodreads.com/author/show.xml?id=4432&key=${GOODREADS_API_KEY}`,
+  () => {}
+);
